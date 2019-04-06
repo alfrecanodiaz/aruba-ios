591d8f4106b8050805ce62a9921ee4d69524add5
Javier Rivarola <javierrivarola91@gmail.com>
2019-04-05 22:52:07 -0400

    Initial Commit

---
+++ Aruba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aruba
+//
+//  Created by Javier Rivarola on 4/5/19.
+//  Copyright © 2019 Javier Rivarola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
